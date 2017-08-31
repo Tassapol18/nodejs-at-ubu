509d56fdb12e74cf96beb07b4871039006e3525d
Tassapol18 <Tassapol.lo.57@ubu.ac.th>
2017-08-31 11:50:28 +0700

    sent Homework

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1 style="color: green;">Hello World!</h1>');
+    res.end('<h1 style="color: green;">Hello World! From Tassapol Lojaras : 5711404826</h1>');
 }).listen(8080);
