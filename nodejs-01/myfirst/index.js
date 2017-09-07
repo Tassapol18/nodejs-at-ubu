4958a7575e9086fc0da2da62b19b339381cb1394
Jetarin7t <jetarin.ta.57@ubu.ac.th>
2017-09-07 09:01:14 +0700

    edit-first

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1 style="color: green;">Hello World! From Tassapol Lojaras : 5711404826</h1><br><h1 style="color: blue;">Hello World! From Jetarin Taloet : 5711407085</h1>');
+    res.end('<h1 style="color: red;">Hello World!</h1><h2 style="color: blue;">From Tassapol Lojaras : 5711404826 </h2>');
 }).listen(8080);
