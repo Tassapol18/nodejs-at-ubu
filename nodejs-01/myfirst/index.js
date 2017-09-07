f66727d29af38c34c20257c8401c323e596fc292
Tassapol18 <Tassapol.lo.57@ubu.ac.th>
2017-09-07 09:27:31 +0700

    edit-tassapol

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1 style="color: red;">Hello World!</h1><h2 style="color: blue;">From Tassapol Lojaras : 5711404826 </h2><h2 style="color: green;">From Jetarin Taloet : 5711407085 </h2>');
+    res.end('<h1 style="color: red;">Hello World!</h1><h2 style="color: blue;">From Tassapol Lojaras : 5711404826 </h2><h2 style="color: green;">From Jetarin Taloet : 5711407085 </h2><h3 style="color:Magenta;">Welcome</h3>');
 }).listen(8080);
